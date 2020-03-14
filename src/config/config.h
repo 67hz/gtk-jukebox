20808159383a06641bf993ef2e4d31087369d79a
Aaron Hinojosa <67hz@protonmail.com>
2020-03-14 03:50:13 -0500

    working config reader

--- src/config/config.h
+++ src/config/config.h
@@ -13,15 +13,14 @@ jukebox_get_configuration ()
   g_autoptr(GError) error = NULL;
   g_autoptr(GKeyFile) key_file = g_key_file_new ();
 
-  if (!g_key_file_load_from_file (key_file, "example.ini", G_KEY_FILE_NONE, &error))
+  if (!g_key_file_load_from_file (key_file, "./example.ini", G_KEY_FILE_NONE, &error))
   {
     if (!g_error_matches (error, G_FILE_ERROR, G_FILE_ERROR_NOENT))
       g_warning("Error loading configuration file: %s", error->message);
     return;
   }
 
-  g_autofree gchar *val = g_key_file_get_string (key_file, "Group Name", "SomeKey", &error);
-  g_printf("test\n\n\n");
+  g_autofree gchar *val = g_key_file_get_string (key_file, "Spotify", "User", &error);
   if (val == NULL &&
       !g_error_matches (error, G_KEY_FILE_ERROR, G_KEY_FILE_ERROR_KEY_NOT_FOUND))
   {
@@ -35,9 +34,6 @@ jukebox_get_configuration ()
   }
 
   g_printf("Found value: %s", val);
-
-  exit(EXIT_SUCCESS);
-
 }
 
 #endif /* ifndef __JUKEBOX_CONFIG_H__ */
